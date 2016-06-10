88b7d1b87fc9704f266fd483c1743b0f3942e64b
Darlight <andres.perdomo09@gmail.com>
2016-06-09 21:17:53 -0600

    Problema #10 En progreso
    P.S: No lo probé. Pienso que falta unos pequeños detalles.

--- prob_10.py
+++ prob_10.py
@@ -1,17 +1,16 @@
 def prob_10(a,b,c):
+	import math
+	d1 = math.sqrt( (( (b[0] - a[0]) **2)) + ((b[1] - a[1]) **2) )
+	d2 = math.sqrt( (( (c[0] - a[0]) **2)) + ((c[1] - a[1]) **2) )
+	d3 = math.sqrt( (( (b[0] - c[0]) **2)) + ((b[1] - c[1]) **2) )
+
 	equi ="Es Equiletaro"
 	isos = "Es Isosceles "
 	esc = "Es Escaleno"
 	
-	if a[0] == b[0] and b[0] == c[0] and a[1] == b[1] and b[1] == c[1]:
+	if d1 == d2 and d2 == d3 and d3 == d1:
 		return equi
-	elif a == b or a == b or b == c:
+	elif d1 == d2 or d2 == d3 or d1 == d3:
 		return isos
-	elif a != b or a != c or b != c:
+	elif d1 != d2 or d1 != d3 or d2 != d3:
 		return esc
-
-
-
-a = (2,2)
-b = (2,2)
-c =	(2,2)
